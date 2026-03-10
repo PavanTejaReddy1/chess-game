ec42e8c0d12c7d5756812e2bceffc0bcc118a4c2
pavantejareddy1 <pavantejareddy85@gmail.com>
2026-03-10 14:28:55 +0530

    Implemented redux part

--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const { login, signup, fetchMe } = require("../controllers/user.controller");
+const { login, signup, fetchMe, logout, refresh } = require("../controllers/user.controller");
 const { verifyAuth } = require("../middlewares/verifyAuth");
 
 const authRouter = express.Router();
 
-authRouter.post("/login", login)
-authRouter.post("/signup", signup)
-authRouter.post("/logout", (req, res) => res.status(200))
-authRouter.post("/refresh", (req, res) => res.status(200))
-authRouter.get("/me", verifyAuth, fetchMe)
+authRouter.post("/login", login);
+authRouter.post("/signup", signup);
+authRouter.post("/logout", logout);
+authRouter.post("/refresh", refresh);
+authRouter.get("/me", verifyAuth, fetchMe);
 
 module.exports = { authRouter };
